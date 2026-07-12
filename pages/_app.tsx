6e10aef03351fd6bcd7afecdfdb99ea021a0d246
crucie <amaymishra100@gmail.com>
2026-07-13 02:29:59 +0530

    updated favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
           <link
             rel="icon"
-            href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'%3E%3Crect width='32' height='32' rx='0' fill='%23ff2d3b'/%3E%3Ctext x='16' y='22' text-anchor='middle' fill='white' font-size='12' font-family='system-ui' font-weight='700'%3ECX%3C/text%3E%3C/svg%3E"
+            href="/favicon.svg"
           />
         </Head>
         <Component {...pageProps} />
